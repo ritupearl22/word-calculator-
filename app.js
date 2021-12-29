5bffa6eca6158b5509a147104829f5b2897459de
ritupearl22 <58977389+ritupearl22@users.noreply.github.com>
2021-12-29 19:23:32 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const wordcount[count, setCount] => useState(0){
+   setCount(count+1);
+ }
+ return(
+ <>
+   <input type="text" id = "char-limit-input">Word limit input</input>
+  <input type="text" id = "word-counter"></input>
+ </>
+ );
+ export default app;
